3cd9880cce119318fb3ad055303e7bcffc55cb65
AbuWaqid-Mir <abuwaqid.mir@outlook.com>
2026-07-27 00:03:32 +0100

    finished base version

--- num_guesser.py
+++ num_guesser.py
@@ -4,14 +4,9 @@ import time
 def intro():
     # Create a title using ASCII
     print("""
-  _   _                 _                  ____                               
- | \ | |_   _ _ __ ___ | |__   ___ _ __   / ___|_   _  ___  ___ ___  ___ _ __ 
- |  \| | | | | '_ ` _ \| '_ \ / _ \ '__| | |  _| | | |/ _ \/ __/ __|/ _ \ '__|
- | |\  | |_| | | | | | | |_) |  __/ |    | |_| | |_| |  __/\__ \__ \  __/ |   
- |_| \_|\__,_|_| |_| |_|_.__/ \___|_|     \____|\__,_|\___||___/___/\___|_|   
-                                                                              
+##### NUMBER GUESSER ######
     """)
-    guesses = 0
+    time.sleep(1)
     print("Welcome to the Number Guesser")
     time.sleep(1) # delay the program by 1 second
     print("I will randomly generate a number between 1 and 100")
@@ -20,23 +15,28 @@ def intro():
     time.sleep(1)
     print("I will give you hints by saying that your guess is too high or too low")
     time.sleep(1)
-    return guesses
 
-def main(guesses):
-    rand_num = random.randint(1, 100)
-    user_guess = int(input("Guess my number from the range 1-100: "))
-    while user_guess != rand_num:
-        if user_guess > rand_num:
-            print("Your number is too high")
-        elif user_guess < rand_num:
-            print("Your number is too low")
-        guesses += 1
-        user_guess = int(input("Guess my number from the range 1-100: "))
-    if user_guess == rand_num:
-        guesses += 1
-        print("Well Done, you guessed it!")
-        time.sleep(1)
-        print("You took", guesses, "guesses")
+def main():
+    guesses = 0
+    rand_num = random.randint(1, 100) # generate a random integer in 1-100 range
+    while True:
+        try:
+            user_guess = int(input("Guess my number from the range 1-100: ")) # user input
+            while user_guess != rand_num:
+                    if user_guess > rand_num:
+                        print("Your number is too high")
+                    elif user_guess < rand_num:
+                        print("Your number is too low")
+                    guesses += 1 # 1 will be added to the guesses variable
+                    user_guess = int(input("Guess my number from the range 1-100: "))
+            if user_guess == rand_num:
+                guesses += 1
+                print("Well Done, you guessed it!")
+                time.sleep(0.5)
+                print("You took", guesses, "guesses")
+                break
+        except ValueError:
+            print("Invalid input - input must be an integer")
 
-guesses = intro()
-main(guesses)
+intro()
+main()
